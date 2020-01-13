d9a6f11c329072800d1cff9f522918ca6cd31858
hamigakitanuki <kakizaki0213@gmail.com>
2020-01-14 02:42:57 +0900

    a

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('get_tag_list',views.get_tag_list,name='get_tag_list'),
-    path('post_tag_inf',views.post_tag_info,name='post_tag_info'),
+    path('post_tag_info',views.post_tag_info,name='post_tag_info'),
 ]
