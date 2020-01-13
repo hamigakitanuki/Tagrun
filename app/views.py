1dd3196cba2c98faf7273a84bb5d34a9649a4783
hamigakitanuki <kakizaki0213@gmail.com>
2020-01-14 02:41:44 +0900

    a

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.db import models
 from django.http.response import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import *
 import random
 
