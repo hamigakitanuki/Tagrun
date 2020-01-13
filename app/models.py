30e1354e3a2ab6006abc048c1e8015b3f22a9c57
hamigakitanuki <kakizaki0213@gmail.com>
2020-01-14 03:41:27 +0900

    a

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class Image_list(models.Model):
     correct = models.IntegerField(default=-1)
 
     def __str__(self):
-        return [self.cate,self.cate,self.sub,str(self.correct)]
+        return self.cate,self.cate,self.sub,str(self.correct)
 
 class Cate(models.Model):
     cate = models.CharField(max_length=30)
